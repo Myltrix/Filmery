e89f85798ef7a518d0a1b08f2b8a3aa82b7e18d1
Myltrix <m1ras.tu1.es2ev@gmail.com>
2026-03-07 22:54:20 +0500

    Update api.js

--- movie-watchlist/client/src/utils/api.js
+++ movie-watchlist/client/src/utils/api.js
@@ -1,38 +1,38 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://movie-watchlist-api.onrender.com/api',
+  baseURL: "https://movie-watchlist-api.onrender.com/api",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export const authAPI = {
-  register: (userData) => api.post('/auth/register', userData),
-  login: (userData) => api.post('/auth/login', userData),
-  getMe: () => api.get('/auth/me'),
+  register: (userData) => api.post("/auth/register", userData),
+  login: (userData) => api.post("/auth/login", userData),
+  getMe: () => api.get("/auth/me"),
 };
 
 export const moviesAPI = {
-  getAll: () => api.get('/movies'),
+  getAll: () => api.get("/movies"),
   getById: (id) => api.get(`/movies/${id}`),
-  create: (movieData) => api.post('/movies', movieData),
+  create: (movieData) => api.post("/movies", movieData),
   update: (id, movieData) => api.put(`/movies/${id}`, movieData),
   delete: (id) => api.delete(`/movies/${id}`),
-  getCommunityMovies: () => api.get('/movies/community'),
+  getCommunityMovies: () => api.get("/movies/community"),
   getComments: (movieId) => api.get(`/movies/${movieId}/comments`),
   addComment: (movieId, commentData) =>
     api.post(`/movies/${movieId}/comments`, commentData),
