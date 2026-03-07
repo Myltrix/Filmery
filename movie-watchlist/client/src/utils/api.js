550846016cf49a250d52cfa6c955ad42805b15b6
Myltrix <m1ras.tu1.es2ev@gmail.com>
2026-03-07 23:59:21 +0500

    Change API base URL to new endpoint

--- movie-watchlist/client/src/utils/api.js
+++ movie-watchlist/client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://movie-watchlist-api.onrender.com/api",
+  baseURL: 'https://filmery.onrender.com/api',
   headers: {
     "Content-Type": "application/json",
   },
