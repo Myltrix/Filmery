98979b5b66f7738db2c0f07e5358ba84a8a37e12
Myltrix <m1ras.tu1.es2ev@gmail.com>
2026-03-08 20:46:09 +0500

    Add community movies and comments routes

--- movie-watchlist/server/routes/movieRoutes.js
+++ movie-watchlist/server/routes/movieRoutes.js
@@ -3,19 +3,28 @@ const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const {
   getMovies,
+  getCommunityMovies,
   getMovieById,
   createMovie,
   updateMovie,
-  deleteMovie
+  deleteMovie,
+  getComments,
+  addComment
 } = require('../controllers/movieController');
 
 router.route('/')
   .get(protect, getMovies)
   .post(protect, createMovie);
 
+router.get('/community', protect, getCommunityMovies);
+
 router.route('/:id')
   .get(protect, getMovieById)
   .put(protect, updateMovie)
   .delete(protect, deleteMovie);
+
+router.route('/:movieId/comments')
+  .get(protect, getComments)
+  .post(protect, addComment);
 
 module.exports = router;
